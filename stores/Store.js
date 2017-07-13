aaa3f01eb8d3054ad9cf1617f3d9167732b681df
mendelB <mendelblesofsky@gmail.com>
2017-07-13 16:10:01 -0400

    Done.

--- stores/Store.js
+++ stores/Store.js
@@ -1,6 +1,26 @@
 'use strict';
 
 class Store {
+	constructor(initialState) {
+		this.state = initialState
+		this.listeners = []
+	}
+
+	getState() {
+		return this.state
+	}
+
+	setState(state) {
+		this.state = state
+		this.listeners.forEach(listener => listener(state))
+	}
+
+	addListener(listener) {
+		this.listeners.push(listener)
+		return () => {
+			this.listeners = this.listeners.filter(l => l !== listener)
+		}
+	}
 }
 
 module.exports = Store;
--- stores/fileStore.js
+++ stores/fileStore.js
@@ -2,7 +2,26 @@
 
 const Store = require('./Store');
 
-class FileStore {
+class FileStore extends Store{
+	updateFile(index, val) {
+		let files = this.getState().slice()
+		files[index] = val
+		this.setState(files)
+	}
+
+	addFile() {
+		this.updateFile(this.state.length, '')
+	}
+
+	removeFile(index) {
+		let files = this.getState().slice()
+		if (files.length > 1) {
+			files.splice(index, 1)
+			this.setState(files)
+		} else {
+			this.updateFile(0, '')
+		}
+	}
 }
 
-module.exports = new FileStore([]);
+module.exports = new FileStore(['']);
